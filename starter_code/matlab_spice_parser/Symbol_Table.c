ce874d8d731759d7baab3fb26c186ea6f7dc3739
yiboliu94 <liuyibo1994@hotmail.com>
2018-03-20 19:49:01 -0700

    Add files via upload

--- starter_code/matlab_spice_parser/Symbol_Table.c
+++ starter_code/matlab_spice_parser/Symbol_Table.c
@@ -37,6 +37,12 @@ int DeviceTableSize;
 
 void Init_Symbol_Tables()
 {
+	int node_entry_size=sizeof(Node_Entry*);
+	int device_entry_size=sizeof(Device_Entry*);
+	NodeTable =(Node_Entry **)malloc(node_entry_size);
+	NodeTableSize = 0;
+	DeviceTable =(Device_Entry **) malloc(device_entry_size);
+	DeviceTableSize = 0;
 }
 
 
@@ -47,56 +53,138 @@ void Destroy_Symbol_Table()
 }
 
 
-void Delete_Node_Table()
+void Delete_Node_Table()	//not sure. need to delete every pointer in NodeTable too?
 {
+	free(NodeTable);
 }
 
 void Delete_Device_Table()
 {
+	 free(DeviceTable);
 }
 
 void Delete_Node_Entry(const char *name)
 {
+	Node_Entry *pre=*NodeTable;
+	if(!strcmp(name,pre->name)){
+        *NodeTable = (*NodeTable)->next;
+        NodeTableSize--;
+        return;
+    }
+    Node_Entry *cur=pre->next;
+	while(cur!=NULL){
+    	if(!strcmp(name,cur->name)){
+            pre->next = cur->next;
+            NodeTableSize--;
+            break;
+        }
+        pre=cur;
+        cur=cur->next;
+    }
 }
 
 void Delete_Device_Entry(const char *name)
 {
 	// NOT IMPLEMENTED!
+	Device_Entry *pre=*DeviceTable;
+	if(!strcmp(name,pre->name)){
+        *DeviceTable = (*DeviceTable)->next;
+        DeviceTableSize--;
+        return;
+    }
+    Device_Entry *cur=pre->next;
+	while(cur!=NULL){
+    	if(!strcmp(name,cur->name)){
+            pre->next = cur->next;
+            DeviceTableSize--;
+            break;
+        }
+        pre=cur;
+        cur=cur->next;
+    }
 }
 
 Node_Entry* Lookup_Node_Entry(const char *name)
 {
-	return  NULL;
+	Node_Entry* cur = *NodeTable;
+	for (int i=0;i!=NodeTableSize;i++){
+		if(!strcmp(name,cur->name)){
+			return cur;
+		}
+		cur=cur->next;
+	}
+	return NULL;
 }
 
 
 Device_Entry* Lookup_Device_Entry(const char *name)
 {
+
+	Device_Entry* cur = *DeviceTable;
+	for (int i=0;i!=DeviceTableSize;i++){
+		if(!strcmp(name,cur->name)){
+			return cur;
+		}
+		cur=cur->next;
+	}
 	return NULL;
 }
 
 
 Node_Entry* Insert_Node_Entry(const char *name)
 {
-	return NULL;
+	int node_entry_size=sizeof(Node_Entry);
+	NodeTableSize++;
+	Node_Entry* n;
+	n=(Node_Entry* )malloc(node_entry_size);
+	n->next=*NodeTable;
+	*NodeTable = n;
+	(*NodeTable)->name=(char*)name;
+	(*NodeTable)->index=0;
+	return *NodeTable;
 }
 
 
 Device_Entry* Insert_Device_Entry(const char *name,  const int numnodes, 
 				Node_Entry **nodelist, const double value)
 {
-	return NULL;
+	int device_entry_size=sizeof(Device_Entry);
+	DeviceTableSize++;
+	Device_Entry* n;
+	n=(Device_Entry*)malloc(device_entry_size);
+	n->next=*DeviceTable;
+	*DeviceTable = n;
+	(*DeviceTable)->name=(char*)name;
+	(*DeviceTable)->numnodes =(int)numnodes;
+	(*DeviceTable)->nodelist = nodelist;
+	(*DeviceTable)->value =(double)value;
+	return *DeviceTable;
 }
 
 
 void Print_Node_Table()
 {
+	Node_Entry *cur = *NodeTable;
+	printf("\nTotal nodes number=%d\n",NodeTableSize);
+	for(int i=0;i!=NodeTableSize;i++){
+		printf("Node%dname=%s\n",cur->index,cur->name);
+		cur=cur->next;
+	} 
 }
-
 
 void Print_Device_Table()
 {
+    Device_Entry *cur = *DeviceTable;
+    printf("\nTotal device number=%d",DeviceTableSize);
+    for(int i=0;i!=DeviceTableSize;i++){
+    	printf("\nDevice=%s,value=%f,node_number=%d\nNodelist Detail:	",cur->name,cur->value,cur->numnodes);
+        for(int ii=0;ii<cur->numnodes;ii++){
+			printf("DeviceNode%d: %s, ",ii,cur->nodelist[ii]->name);
+		}
+		cur = cur->next;
+    }
 }
+
 
 
 int NameHash(const char *name,  int tsize)
